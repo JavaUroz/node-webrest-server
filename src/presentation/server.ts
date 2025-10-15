5ec496eb0eb1be14c7537777861c97dbce5cfdb6
JavaUroz <urozjavier@gmail.com>
2025-10-15 16:08:40 -0300

    package.json actualizado

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -33,7 +33,7 @@ export class Server {
         });
 
         this.app.listen(this.port, () => {
-            console.log(`Server running on port http://localhost:${ this.port }`)
+            console.log(`Server running on port: http://localhost:${ this.port }`)
         });
     }
 }
